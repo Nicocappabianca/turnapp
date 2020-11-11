29410bbc6af434bbea50eaf136e325b64f7ba4c2
nicocappabianca <nicolas.cappabianca@gmail.com>
2020-11-10 21:33:26 -0300

    code reorganization

--- html/header.php
+++ html/header.php
@@ -22,9 +22,6 @@
         <a class="navbar-brand" href="#">
             <img src="../assets/icons/logo.png" width="80" class="d-inline-block align-top" alt="TurnApp">
         </a>
-        <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button> -->
         <button class="navbar-toggler collapsed ml-auto" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="icon-bar top-bar"></span>
             <span class="icon-bar middle-bar"></span>
