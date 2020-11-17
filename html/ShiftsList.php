17edfbeb88086a40f9a31da70a71357d226f0a01
nicocappabianca <nicolas.cappabianca@gmail.com>
2020-11-16 22:38:26 -0300

    remove breaks from switch

--- html/ShiftsList.php
+++ html/ShiftsList.php
@@ -7,18 +7,18 @@
 <?php 
     function toMonth($monthNumber) { 
         switch($monthNumber) { 
-            case 1: return 'Enero'; break;
-            case 2: return 'Febrero'; break;
-            case 3: return 'Marzo'; break;
-            case 4: return 'Abril'; break;
-            case 5: return 'Mayo'; break;
-            case 6: return 'Junio'; break;
-            case 7: return 'Julio'; break;
-            case 8: return 'Agosto'; break;
-            case 9: return 'Septiembre'; break;
-            case 10: return 'Octubre'; break;
-            case 11: return 'Noviembre'; break;
-            case 12: return 'Diciembre'; break; 
+            case 1: return 'Enero'; 
+            case 2: return 'Febrero'; 
+            case 3: return 'Marzo'; 
+            case 4: return 'Abril'; 
+            case 5: return 'Mayo'; 
+            case 6: return 'Junio'; 
+            case 7: return 'Julio'; 
+            case 8: return 'Agosto'; 
+            case 9: return 'Septiembre'; 
+            case 10: return 'Octubre'; 
+            case 11: return 'Noviembre'; 
+            case 12: return 'Diciembre';  
         }
     }
 ?>
