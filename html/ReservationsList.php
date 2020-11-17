e5af4feea034f31899c1836bd1a5b012a29d5471
galarcelucas@gmail.com <galarcelucas@gmail.com>
2020-11-16 21:17:41 -0300

    Reservation list joins

--- html/ReservationsList.php
+++ html/ReservationsList.php
@@ -1,15 +1,18 @@
 <link rel="stylesheet" href="../assets/css/home.css">
 
 <section id="home" class="text-center pt-4">
-    <h1>HACE TUS RESERVAS MÁS FÁCIL</h1>
-    <h3>Elegí la empresa, el día, el horario ¡y listo!</h3>
+    <h1>TUS RESERVAS</h1>
     <div class="container mt-4">
         <?php foreach($this->reservations as $reservation): ?>
             <div class="swiper-slide company-item">
                 <div class="card">
+                    <img class="card-img-top" src="<?= $reservation['url_image']?>" alt="<?= $reservation['name'] ?>">
                     <div class="card-body">
-                        <h5 class="card-title"><?= $reservation['id_user'] ?></h5>
-                        <p class="card-text"><?= $reservation['id_turn'] ?></p>
+                        <h5 class="card-title"><?= $reservation['name'] ?></h5>
+                        <p class="card-text">Fecha: <?= $reservation['date_time'] ?></p>
+                        <p class="card-text">Dirección: <?= $reservation['address'] ?></p>
+                        <p class="card-text">Precio: <?= $reservation['price'] ?></p>
+
                     </div>
                 </div>
             </div>
