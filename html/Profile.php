07c8be6ba8788e73b2017d3f4dfade71b37be7f8
nicocappabianca <nicolas.cappabianca@gmail.com>
2020-12-02 00:14:54 -0300

    profile responsive fix

--- html/Profile.php
+++ html/Profile.php
@@ -12,10 +12,10 @@
 <section id="profile">
     <div class="container">
         <div class="row">
-            <div class="col-12 offset-md-3 col-md-2 text-center text-md-right">
+            <div class="col-12 offset-lg-3 col-lg-2 text-center text-lg-right">
                 <img class="profile-picture" src="https://www.gravatar.com/avatar/<?= $this->gravatar ?>?d=https://www.weact.org/wp-content/uploads/2016/10/Blank-profile.png" alt="<?= $this->userName .' '. $this->userSurname ?>">
             </div>
-            <div class="col-12 col-md-6 text-center text-md-left pt-3 pt-lg-0 profile-info">
+            <div class="col-12 col-lg-6 text-center text-lg-left pt-3 pt-lg-0 profile-info">
                 <h2><?= $this->userName ?> <?= $this->userSurname?></h2>
                 <p class="profile-email"><?= $this->userEmail ?></p>
             </div>
