9cad8111cb8ad4314a8e5109b7be21acba282add
galarcelucas@gmail.com <galarcelucas@gmail.com>
2020-12-01 23:42:13 -0300

    date validation

--- controllers/CreateShift.php
+++ controllers/CreateShift.php
@@ -7,6 +7,11 @@ if(!isset($_SESSION)) session_start();
 if( (!empty($_POST['date'])) && (!empty($_POST['time'])) ) {
     $date = $_POST['date'];
     $time = $_POST['time'];
+    $today = strtotime(date("Y-m-d",time()));
+
+    if($date < $today){
+        header('Location: Admin.php');
+    }
     
     $shiftsModel = new Shifts(); 
     $shiftsModel->createShift($_SESSION['companyId'], $date, $time); 
